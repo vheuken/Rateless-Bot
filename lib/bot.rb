eacd76f7adc35a9537d0d95494d3ebcd392aacdb
Vincent Heuken <me@vincentheuken.com>
2014-11-26 16:53:21 -0800

    removed greet

--- lib/bot.rb
+++ lib/bot.rb
@@ -3,7 +3,6 @@ require 'yaml'
 require_relative 'plugins/coin_flipper'
 require_relative 'plugins/dice_roller'
 require_relative 'plugins/dead_screamer'
-require_relative 'plugins/greet'
 require_relative 'plugins/help'
 require_relative 'plugins/magic_eight_ball'
 require_relative 'plugins/video_title'
@@ -17,7 +16,6 @@ bot = Cinch::Bot.new do
     c.plugins.plugins = [CoinFlipper,
 			 DeadScreamer,
 			 DiceRoller,
-			 Greet,
 			 Help,
 			 MagicEightBall,
                          VideoTitle]
