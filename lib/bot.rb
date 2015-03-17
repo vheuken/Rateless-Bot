2e4515f5e8375160747832c523381877e9f6f224
Vincent Heuken <me@vincentheuken.com>
2015-03-17 01:38:31 -0700

    removed dead_screamer

--- lib/bot.rb
+++ lib/bot.rb
@@ -2,7 +2,6 @@ require 'cinch'
 require 'yaml'
 require_relative 'plugins/coin_flipper'
 require_relative 'plugins/dice_roller'
-require_relative 'plugins/dead_screamer'
 require_relative 'plugins/help'
 require_relative 'plugins/magic_eight_ball'
 require_relative 'plugins/video_title'
@@ -14,7 +13,6 @@ bot = Cinch::Bot.new do
     c.channels = ["#rateless"]
 
     c.plugins.plugins = [CoinFlipper,
-			 DeadScreamer,
 			 DiceRoller,
 			 Help,
 			 MagicEightBall,
