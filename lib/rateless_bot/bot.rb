ed7121385cae99e16381f7216afc29f61e3d1330
Vincent Heuken <me@vincentheuken.com>
2015-03-24 18:54:46 -0700

    command line now works

--- lib/rateless_bot/bot.rb
+++ lib/rateless_bot/bot.rb
@@ -6,17 +6,3 @@ require_relative 'plugins/help'
 require_relative 'plugins/magic_eight_ball'
 require_relative 'plugins/video_title'
 
-bot = Cinch::Bot.new do
-  configure do |c|
-    c.nick = "Rateless-Bot"
-    c.server = "irc.rizon.net"
-    c.channels = ["#rateless"]
-
-    c.plugins.plugins = [CoinFlipper,
-			 DiceRoller,
-			 Help,
-			 MagicEightBall,
-                         VideoTitle]
-  end
-end
-
